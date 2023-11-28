b793a68583560c8cb2126113a37b2492e46f7863
shukhratjon717 <shukhratjon0717@gmail.com>
2023-11-28 15:13:02 +0900

    new activity added

---
+++ script.js
@@ -0,0 +1,41 @@
+var male = prompt("what is your name?")
+var female = prompt("what is your name?")
+
+var friendship = Math.random() * 100 ;
+
+friendship = Math.floor(friendship)+ 1;
+
+if (frienship > 100) {
+    alert("Your friendship dcore is " + frienship + "% you are best friends")
+} else {
+        alert("Your friendship dcore is " + frienship + "% ")
+}
+
+alert("Your friendship is " + friendship + "%")
+
+
+
+
+function getBMI (weight, height) {
+    var weight =  prompt("What is your weight?")
+var height = prompt("What is your weight?")
+    
+    var bmi = weight/(height * height);
+
+    var whole = Math.round(bmi)
+
+    if (whole < 18.5) {
+        console.log("Your BMI is " + bmi + "so you are underweight")
+        
+    console.log("your BMI is " + whole + " You are normal weight" )
+    
+    } else {
+        console.log('Your BMI is ' + whole + " You are overweiht")
+    }
+    
+    return whole;
+}
+
+
+alert(whole)
+        
