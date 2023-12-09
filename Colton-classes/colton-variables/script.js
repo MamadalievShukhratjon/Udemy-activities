5017bd2704bb3f800638789a1d81824bf2c9310b
shukhratjon717 <shukhratjon0717@gmail.com>
2023-12-09 19:44:10 +0900

    String methods have been added

--- Colton-classes/colton-variables/script.js
+++ Colton-classes/colton-variables/script.js
@@ -8,4 +8,27 @@
 //  these two are the same thing
  score += 10 
 
+//   STRING
 
+// Strings are another primitive type in javascript. They represent texxt and must be wrapped in quotes ("", ''). Single quote or double quotes are fine to use.
+
+let place = 'School' || "school"
+
+// Strings are index and each character has corresponding index (positional number).
+
+let animal = 'cat'
+
+//  STRING METHODS
+// Methods are built in actions, we can perform with individual strings. they help us to do things like:
+// Searching within a string
+// replacing part of a string
+// Changing the casing of a string.
+
+// toUpperCase() => It makes all text upper letters
+// toLowerCase() => It makes all text lower letters
+// trim() = it removes the white spaces aroung the string it text.
+
+let msg = '     Leave Me Alone    '
+console.log(msg.toUpperCase())
+console.log(msg.toLowerCase())
+console.log(msg.trim())
