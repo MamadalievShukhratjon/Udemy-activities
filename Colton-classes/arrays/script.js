ec684ce2ad49ed41a17452f0f862ddc2b3ad1c83
shukhratjon717 <shukhratjon0717@gmail.com>
2023-12-18 20:40:31 +0900

    new file added

---
+++ Colton-classes/arrays/script.js
@@ -0,0 +1,5 @@
+    // Javascript Arrays
+    // Introduction to arrays
+    // Array objects enabels storing a collection of multiple items under a single variable name and has a member for performing common array operations
+    // creating array
+    let color = ['red', 'green', 'blue']
