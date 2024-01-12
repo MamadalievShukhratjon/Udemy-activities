f023f05e90485e976e7a1f389bce6488c8535fad
shukhratjon717 <shukhratjon0717@gmail.com>
2024-01-12 19:11:35 +0900

    practice file added

--- Colton-classes/domjs/index.js
+++ Colton-classes/domjs/index.js
@@ -69,3 +69,13 @@ console.log(link.href);
 
 const donetoDoes = document.querySelector('.done')
 const checkbox = document.querySelectorAll('input[type="checkbox"]')
+
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']; //PLEASE DON'T CHANGE THIS LINE!
+
+//YOU CODE GOES HERE:
+const spans = document.querySelectorAll('span')
+
+for (let i= 0; i < spans.length; i++) {
+    
+    spans[i].style.color = colors[i]
+}
